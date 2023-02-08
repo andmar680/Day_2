1cde0b9902d981e89ed364ed967f239969818c17
andmar680 <andmar680@gmail.com>
2023-02-07 20:00:20 -0600

    explaination added

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,6 +1,7 @@
 
 
 function toRoman(num) {
+    // hash table initialized to contain keys with respect to roman Nums
     const hashtable = {
         M:  1000,
         CM: 900,
@@ -16,13 +17,17 @@ function toRoman(num) {
         IV: 4,
         I:  1,
       };  
-      let result = '';
-    //   
-      for (key in hashtable) {
-        result += key.repeat(Math.floor(num / hashtable[key]));
-        num %= hashtable[key];
+      let result = ''; // string is used, and when letter to add is found, we're able to add it to 'result'
+    
+      for (key in hashtable) {// loop through each key. 
+        result += key.repeat(Math.floor(num / hashtable[key])); //first divide whole num key and floored for remainder
+        //if key > num then num will be floored to 0 and nothing happens
+        // if key <= num then once it is floored, it'll give us how many times key can divide num evenly, that number is used
+        // in key.repeated(floored number), then is added to result
+        num %= hashtable[key]; // num is updated by modulus key.  Remainder keeps going for other hashtable keys
+        //If the key was bigger then nothing happens.
       }
       
       return result;
 }
-exports.toRoman = toRoman
+exports.toRoman = toRoman //exports passes roman () to romanNumeralsSpec.js when invoked
--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -1,6 +1,7 @@
-var rn = require("./romanNumerals");
+var rn = require("./romanNumerals");//require() takes in a file and since 
+//romanNumeralsSpecs.js is along the same path as romanNumerals.js
 
 // console.log(rn.toRoman)
-console.log(rn.toRoman(1) === 'I');
-console.log(rn.toRoman(3) === 'III');
-console.log(rn.toRoman(4) === 'IV');
+console.log(rn.toRoman(1) === 'I');// this line invokes toRoman in other file because
+console.log(rn.toRoman(3) === 'III'); // rn was declared on line 1 of this file and
+console.log(rn.toRoman(4) === 'IV'); // initialized toRoman exporting towards this file.
